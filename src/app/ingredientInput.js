01e9360c7071ad77e0156a25dcb22ce19eb1f18a
youssefEchadySfaxi <youssefechadysfaxi@gmail.com>
2025-03-20 08:38:20 +0100

    style: added tailwind styling

--- src/app/ingredientInput.js
+++ src/app/ingredientInput.js
@@ -35,7 +35,8 @@ const IngredientInput = () => {
         placeholder="Add ingredient..." 
         autoFocus 
         value={ingredient}
-        onChange={handleInputChange} 
+        onChange={handleInputChange}
+        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
       />
       
       {suggestions.length > 0 && (
@@ -51,13 +52,20 @@ const IngredientInput = () => {
           ))}
         </ul>
       )}
-      <div>
+      <div className='ingredient-box'>
         {currentIngredients.map((item) => (
-          <div key={item.id} className='ingredient-box'>
-            <p>{item.name}</p>
+          <div key={item.id}>
+            <div className="bg-blue-100 text-blue-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded-sm dark:bg-blue-900 dark:text-blue-300">{item.name}</div>
           </div>
         ))}
       </div>
+
+        <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+            Get recipes
+            <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+            </svg>  
+        </button>
     </div>
   );
 };
--- src/app/page.js
+++ src/app/page.js
@@ -2,25 +2,20 @@ import Image from "next/image";
 import IngredientInput from './ingredientInput';
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        
+    <div className="grid place-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-[32px] items-center h-full justify-center">
         <nav className="flex gap-4 items-center">
-          <Image src="/ElFrigo.svg" alt="El Frigo logo" width={15} height={38} priority/>
-          <p>El Frigo</p>
+          <Image src="/ElFrigo.svg" alt="El Frigo logo" width={15} height={38} priority />
+          <p><b>El Frigo</b></p>
         </nav>
         
-        <p className="description">Your favorite cooking partner.</p>
-        <h1>Tell us what’s in your fridge, <br/>we’ll tell you what to cook!</h1>
-
-        <IngredientInput/>        
-
-        <button className="btn-primary">Get recipes</button>
+        <p className="description text-center">Your favorite cooking partner.</p>
+        <h1 className="text-center mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl">
+          Tell us what’s in your fridge, <br />we’ll tell you what to cook!
+        </h1>
+        
+        <IngredientInput/>
       </main>
-
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <p>&copy; 2025 El Frigo. All rights reserved</p>
-      </footer>
     </div>
   );
 }
