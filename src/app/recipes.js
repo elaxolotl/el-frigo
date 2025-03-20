48536904554067ac0632d43d21f8d58a72da22fa
youssefEchadySfaxi <youssefechadysfaxi@gmail.com>
2025-03-20 12:14:54 +0100

    style: add tailwind styling to recipe gallery

--- src/app/recipes.js
+++ src/app/recipes.js
@@ -6,14 +6,14 @@ const RecipesGallery = ({ recipes }) => {
   return (
     <div>
       <h1>Recipes Gallery</h1>
-      <div className="gallery">
         {recipes.map((recipe) => (
-          <div key={recipe.id} className="recipe-card">
-            <h2>{recipe.title}</h2>
-            <img src={recipe.image} alt={recipe.title} />
-          </div>
+        <div key={recipe.id}  className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow-sm flex flex-col items-center mb-4 p-4">
+                <img className='w-24 h-24 mb-3 rounded-full shadow-lg' src={recipe.image} alt={recipe.title} />
+                <h5 className="mb-1 text-xl font-medium text-gray-900 text-center">{recipe.title}</h5>
+                <span class="text-sm text-gray-500">Used ingredients: {recipe.usedIngredientCount}</span>
+                <span class="text-sm text-gray-500">Missed ingredients: {recipe.missedIngredientCount}</span>
+        </div>
         ))}
-      </div>
     </div>
   );
 };
